23e7394be95de0204f14a71dd6e975e3e391c6c1
Ahmed <dev.ahmednabil@gmail.com>
2020-05-31 20:56:57 +0200

    localization test

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,6 @@
 import moment from 'moment-hijri';
 import {getHMonthDaysInYear} from './utils';
 
-// export const weekDays = ['Ah', 'It', 'Th', 'Ar', 'Kh', 'Ju', 'Sa'];
-
 export const hijiriDateFormat = 'iYYYY/iM/iD';
 export const gregorianDateFormat = 'YYYY/M/D';
 
@@ -11,65 +9,8 @@ export const calendarTypes = {
   gregorian: 'gregorian',
 };
 
-// export const months = [
-//   'Muharram',
-//   'Safar',
-//   "Rabi' al-Awwal",
-//   "Rabi' al-Thani",
-//   'Jumada al-Ula',
-//   'Jumada al-Alkhirah',
-//   'Rajab',
-//   'Sha’ban',
-//   'Ramadhan',
-//   'Shawwal',
-//   'Thul-Qi’dah',
-//   'Thul-Hijjah',
-// ];
-// const months = [
-//   'January',
-//   'February',
-//   'March',
-//   'April',
-//   'May',
-//   'June',
-//   'July',
-//   'August',
-//   'September',
-//   'October',
-//   'November',
-//   'December',
-// ];
-
-export const hMonthsShort = [
-  'Muh',
-  'Saf',
-  'Rab-I',
-  'Rab-II',
-  'Jum-I',
-  'Jum-II',
-  'Raj',
-  'Sha',
-  'Ram',
-  'Shw',
-  'Dhu-Q',
-  'Dhu-H',
-];
-
-export const gMonthsShort = [
-  'Jan',
-  'Feb',
-  'Mar',
-  'Apr',
-  'May',
-  'Jun',
-  'Jul',
-  'Aug',
-  'Sept',
-  'Oct',
-  'Nov',
-  'Dec',
-];
-
+export const hMonthsShort = moment.iMonthsShort();
+export const gMonthsShort = moment.monthsShort();
 export const weekDays = moment.weekdaysMin();
 export const hNDays = getHMonthDaysInYear();
 export const gNDays = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
